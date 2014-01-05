ac2df784295cbf718d999dbc98b34158ddbc8f2c
Thomas W Nyberg <tomnyberg@gmail.com>
2014-01-04 19:55:13 -0800

    Being IRQ and PIT implimentation

--- src/isr.h
+++ src/isr.h
@@ -4,6 +4,24 @@
 //          Rewritten for JamesM's kernel development tutorials.
 //
 
+// A few defines to make life a little easier
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+
 struct registers {
     uint32_t ds;                  // Data segment selector
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
@@ -12,3 +30,9 @@ struct registers {
 } __attribute__((packed));
 
 typedef struct registers registers_t;
+
+// Enables registration of callbacks for interrupts or IRQs.
+// For IRQs, to ease confusion, use the #defines above as the
+// first parameter.
+typedef void (*isr_t)(registers_t);
+void register_interrupt_handler(u8int n, isr_t handler);
